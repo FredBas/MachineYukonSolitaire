fe571d8da00ef9327ecb04b204793bc0baad9c6e
Frederik Bastrup <s235117@dtu.dk>
2024-04-24 13:44:54 +0200

    Made a line separator

--- Commands.c
+++ Commands.c
@@ -27,6 +27,7 @@ void printUIMessages(char *lastCommand, char *message) {
 }
 
 void initialTableauPrinter() {
+    printf("__________________________________________________________________________________");
     printf("\nC1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
     printf("  \t  \t  \t  \t  \t  \t  \t\t[]\tF1\n");
     printf("  \t  \t  \t  \t  \t  \t  \t\n");
